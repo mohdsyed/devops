8ebe294f22aac72b7c8ef0358ec818bb6e8f92b8
mohd syed <psyed968@gmail.com>
2019-11-09 04:43:26 -0600

    updates

--- aws/install.sh
+++ aws/install.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-sudo yum -y install java-1.8.0*
+#step - 1 install java 
+sudo yum -y install wget java-1.8.0*
+cd /etc/profile.d 
+sudo wget https://raw.githubusercontent.com/mohdsyed/devops/master/aws/java.sh
+sudo chmod a+x java.sh 
+sudo source /etc/profile.d/java.sh
+
+#adding repository to linux 
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+#isntalling jenkins 
+
+sudo yum -y install jenkins
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+
+
 
